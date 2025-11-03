3aa32d09c46c3c278406e8bd83fc5df98d42e0af
carminerizzi91-spec <carminerizzi91@gmail.com>
2025-11-03 14:04:29 +0100

    add: esercizio 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 // Chiedere all’utente di inserire una parola
-const parola = prompt("Inserisci parola")
+let parola = prompt("Inserisci parola");
 // Creare una funzione per capire se la parola inserita è palindroma (NON usare split, reverse e join)
+function palindroma(parola) {
+    let lunghezza = parola.length;
+    for (let i = 0; i < lunghezza / 2; i++) {
+        if (parola[i] !== parola[lunghezza - i - 1]) {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+// Verifico se la parola è palindorma
+
+if (palindroma(parola)) {
+    console.log("La parola è palindroma")
+    alert("La parola è palindroma")
+} else {
+    console.log("La parola non è palindroma")
+    alert("La parola non è palindroma")
+}
